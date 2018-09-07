317c8673f7f2f2bdf103a6f517d01c5024fff0ed
Pochao Yang <stevenyang0620@hotmail.com>
2018-09-07 15:17:08 -0700

    pl - use ant-design List, Checkbox, Layout to show list
    add jsconfig.json to get rid of the stupid warnings

--- src/TodoListView.js
+++ src/TodoListView.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import './TodoListView.css';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import { List } from "antd";
+import TodoView from './TodoView';
 
-@observer 
+@observer
 export default class TodoListView extends Component {
-    render() {
-        return(
-            <div>
-                {this.props.todoListStore.todoList.map(todo => <TodoView todo={todo} key={todo.id}/>)}
-            </div>
-        );
-    }
-}
+  renderTodo = item => <TodoView todo={item} />
 
-const TodoView = observer(({todo}) =>
-    <li>
-        <input
-            type="checkbox"
-            checked={todo.finished}
-            onClick={() => todo.finished = !todo.finished}
-        /><span className={todo.finished ? 'strike' : ''}>{todo.title}</span>
-    </li>
-)
+  render() {
+    const { todoList } = this.props.todoListStore;
+
+    return <List bordered dataSource={todoList} renderItem={this.renderTodo} />;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Todo, TodoListStore } from './stores/TodoListStore';
+import { dummyStore } from './stores/dummyTodos';
 
-const todo1 = new Todo('First Task');
-const todo2 = new Todo('Second Task');
-const todo3 = new Todo('Third Task');
-
-const todoListStore = new TodoListStore();
-let todos = {};
-todos[todo1.id] = todo1;
-todos[todo2.id] = todo2;
-todos[todo3.id] = todo3;
-todoListStore.setTodos(todos);
-
-ReactDOM.render(<App todoListStore={todoListStore} />, document.getElementById('root'));
+ReactDOM.render(<App todoListStore={dummyStore} />, document.getElementById('root'));
 registerServiceWorker();
